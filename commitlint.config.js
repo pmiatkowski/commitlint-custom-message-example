63fd60ed84cc732b057978a30c4b1e6834db1a76
Pawel Miatkowski <pawelmiatkowski@gmail.com>
2021-06-29 21:45:38 +0200

    Dupa

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,17 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    parserOpts: {
-        headerPattern: /^(\w*)\/(\w*)\s(.*)$/,
-        headerCorrespondence: ['type', 'scope', 'subject'],
-    },
-    rules: {
-        'type-enum': [2, 'always', ['foo']],
-        'type-case': [2, 'always', ['lower-case']],
-        'scope-enum': [2, 'always', []],
-        'scope-case': [2, 'always', ['upper-case', 'kebab-case']],
-        'scope-empty': [2, 'never'],
-        'subject-case': [2, 'always', ['lower-case', 'sentence-case', 'start-case']],
-        'subject-empty': [2, 'never'],
-        'header-case': [1, 'always', ['lower-case']]
-    }
+    // parserOpts: {
+    //     headerPattern: /^(\w*)\/(\w*)\s(.*)$/,
+    //     headerCorrespondence: ['type', 'scope', 'subject'],
+    // },
+    // rules: {
+    //     'type-enum': [2, 'always', ['foo']],
+    //     'type-case': [2, 'always', ['lower-case']],
+    //     'scope-enum': [2, 'always', []],
+    //     'scope-case': [2, 'always', ['upper-case', 'kebab-case']],
+    //     'scope-empty': [2, 'never'],
+    //     'subject-case': [2, 'always', ['lower-case', 'sentence-case', 'start-case']],
+    //     'subject-empty': [2, 'never'],
+    //     'header-case': [1, 'always', ['lower-case']]
+    // }
 };
