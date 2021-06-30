d67b834ba40f91d64a4a17d501e267c13b5483d8
Pawel Miatkowski <pawelmiatkowski@gmail.com>
2021-06-30 22:40:39 +0200

    feat/NJI: create example with commitlint custom message parser and husky 6

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,44 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    // parserOpts: {
-    //     headerPattern: /^(\w*)\/(\w*)\s(.*)$/,
-    //     headerCorrespondence: ['type', 'scope', 'subject'],
-    // },
-    // rules: {
-    //     'type-enum': [2, 'always', ['foo']],
-    //     'type-case': [2, 'always', ['lower-case']],
-    //     'scope-enum': [2, 'always', []],
-    //     'scope-case': [2, 'always', ['upper-case', 'kebab-case']],
-    //     'scope-empty': [2, 'never'],
-    //     'subject-case': [2, 'always', ['lower-case', 'sentence-case', 'start-case']],
-    //     'subject-empty': [2, 'never'],
-    //     'header-case': [1, 'always', ['lower-case']]
-    // }
+    plugins: ['commitlint-plugin-function-rules'],
+    rules: {
+        'type-empty': [0],
+        'subject-empty': [0],
+        'type-enum': [0],
+        'function-rules/type-enum': [
+        2,
+        'always',
+        parsed => {
+            /**
+             * Matching examples:
+             * @template **type/JIRATICKET-NUMBER: Message**
+             * @example "fix/TOLO-666: In modal component fix title translation"
+             * @example "refactor/NJI: change naming convention, add rules to eslint config file"
+             */
+            const headerRegex = /^(feat|(hot)?fix|build|ci|docs|refactor|revert|style|test|merge|bump)\/(([A-Z]+-[0-9]+)|(NJI)):\s.*(([\n\r])?.*){0,}$/
+
+            /**
+             * Match: bump/release-X.Y[.Z][: optional message]
+             */
+            const bumpHeaderRegex = /^(bump)\/(release-\d+\.\d+(\.\d+)?)(:\s.*)?/
+            const isHeaderValid = parsed.header.match(headerRegex) || parsed.header.match(bumpHeaderRegex);
+            if (isHeaderValid) {
+                return [true]
+            }
+
+            return [false, `Your message must match these patterns:
+                ${headerRegex}
+                ${bumpHeaderRegex}
+
+            Template: type/(JIRA-NUMBER|NJI|release-X.Y): your description
+            Types: feat, hotfix, fix, build, ci, docs, refactor, revert, style, test, merge, bump
+
+            Examples:
+            # fix/TOLO-666: modal component - fix title translation
+            # refactor/NJI: no jira issue example
+            # bump/release-10.2: description in this case is optional
+            # bump/release-10.2
+            `]
+        }
+        ]
+    }
 };
